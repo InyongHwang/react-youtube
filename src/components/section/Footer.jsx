f5ec512a6806318eb7adfcde0ecc2a8b06bc1f09
hiy53 <hiy@miraeclimate.com>
2024-07-04 14:40:56 +0900

    메인 콘텐츠 작업

--- src/components/section/Footer.jsx
+++ src/components/section/Footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <footer id='footer' role='contentinfo'>
-        Footer
-    </footer>
-  )
+    return (
+        <footer id='footer' role='contentinfo'>
+            <a href="mailto:hiy1934@gmail.com" rel='noopenner noreferrer'>
+                hiy1934@gmail.com
+            </a>
+        </footer>
+    )
 }
 
 export default Footer
