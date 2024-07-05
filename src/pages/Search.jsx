fb5448a6cdca0d4aec3425439d46b6880a9fa02f
hiy53 <hiy@miraeclimate.com>
2024-07-05 12:16:47 +0900

    Rapid API 및 더보기 설정하기

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,41 +1,56 @@
 import React, { useEffect, useState } from 'react'
-import Main from '../components/section/Main'
 import { useParams } from 'react-router-dom'
-import VideoSearch from '../components/videos/VideoSearch';
+import Main from '../components/section/Main'
+
+import VideoSearch from '../components/videos/VideoSearch'
+import { fetchFromAPI } from '../utils/api'
 
 const Search = () => {
-
     const { searchId } = useParams();
     const [ videos, setVideos ] = useState([]);
-
-    const apiKey = process.env.REACT_APP_YOUTUBE_API_KEY;
-    console.log(apiKey)
-
+    const [ nextPageToken, setNextPageToken ] = useState(null); // 다음 페이지 토큰
+    const [ loading, setLoading ] = useState(true);
     
-    const a = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=48&q=${searchId}&type=video&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
-    console.log("AAAAAAAAAAAAAAAAAAAAAAAAA")
-    console.log(a)
-
     useEffect(() => {
-        fetch(
-            `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=48&q=${searchId}&type=video&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`,
-        )
-        .then(response => response.json())
-        .then(result => {
-            console.log(result);
-            setVideos(result.items)
-        })
-        .catch(error => console.log(error));
-        }, [searchId]);
+        setVideos([]); // 검색어가 변경되면 videos 상태 초기화
+        fetchVideos(searchId);
+        setLoading(true);
+    }, [searchId]);
+
+    const fetchVideos = (query, pageToken = '') => {
+        fetchFromAPI(`search?part=snippet&q=${query}&pageToken=${pageToken}`)
+            .then((data) => {
+                setNextPageToken(data.nextPageToken);
+                setVideos((prevVideos) => [...prevVideos, ...data.items]);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.log('Error fetching data:', error);
+                setLoading(false);
+            });
+    };
+
+    const handleLoadMore = () => {
+        if (nextPageToken) {
+            fetchVideos(searchId, nextPageToken);
+        }
+    }
+
+    const searchPageClass = loading ? 'isLoading' : 'isLoaded';
 
     return (
         <Main 
             title = "유투브 검색"
-            description="유튜브 검색 결과 페이지입니다."
-        >
-            <section id='searchPage'>
-                <div className='video__inner search'>
-                    <VideoSearch videos={videos}/>
+            description="유튜브 검색 결과 페이지입니다.">
+            
+            <section id='searchPage' className={searchPageClass}>
+                <div className="video__inner search">
+                    <VideoSearch videos={videos} />
+                </div>
+                <div className="video__more">
+                    {nextPageToken && (
+                        <button onClick={handleLoadMore}>더 보기</button>
+                    )}
                 </div>
             </section>
         </Main>
