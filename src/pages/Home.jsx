0d6b160a08807219c56745415b5908f7aa348e4e
hiy53 <hiy@miraeclimate.com>
2024-07-05 10:32:30 +0900

    Swiper 이미지 슬라이드 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ import Today from '../components/contents/Today'
 import Developer from '../components/contents/Developer'
 import VideoSlider from '../components/videos/VideoSlider'
 
+import { todayText } from '../data/today'
+import { developerText } from '../data/developer';
 import { webdText } from '../data/webd'
 import { websiteText } from '../data/website'
 import { gsapText } from '../data/gsap'
@@ -16,8 +18,8 @@ const Home = () => {
             title='웹스토리보이 유튜브' 
             description='웹스토리보이 유튜브 채널에 오신것을 환영합니다.'
         >
-            <Today />
-            <Developer />
+            <Today videos={todayText} id="today"/>
+            <Developer id={'developer'} title={'😪 추천 개발자를 소개합니다.'} videos={developerText}/>
             <VideoSlider id={'webd'} title={'😮 웹디자인기능사 준비는 이걸로!'} videos={webdText}/>
             <VideoSlider id={'website'} title={'😛 웹표준 사이트 만들기 기초 다지기'} videos={websiteText}/>
             <VideoSlider id={'gsap'} title={'🤓 GSAP 패럴랙스 효과를 하고 싶다면!'} videos={gsapText}/>
